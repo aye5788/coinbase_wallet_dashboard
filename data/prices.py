97965bcf72348706d4a1e3cd1a7d0d1aee168d2c
aye5788 <166234294+aye5788@users.noreply.github.com>
2025-12-31 12:08:59 -0500

    Update prices.py

--- data/prices.py
+++ data/prices.py
@@ -1,1 +1,14 @@
+import requests
+from utils.cache import cache
+
+@cache(ttl=300)
+def get_prices(ids):
+    r = requests.get(
+        "https://api.coingecko.com/api/v3/simple/price",
+        params={
+            "ids": ",".join(ids),
+            "vs_currencies": "usd"
+        }
+    )
+    return r.json()
 
